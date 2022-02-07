8e31f546dd202b6c0daedb81c8c2c137f3fcf40b
max911z <80741988+max911z@users.noreply.github.com>
2022-02-07 19:48:41 +0700

    init commit

---
+++ calculatorSwiftUI/calculatorSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculatorSwiftUI
+//
+//  Created by Максим Неверовский on 07.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calculatorSwiftUI/calculatorSwiftUI/calculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorSwiftUIApp.swift
+//  calculatorSwiftUI
+//
+//  Created by Максим Неверовский on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct calculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
